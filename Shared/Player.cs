7d3dc135f8c999afd01201ab1ffec28cd47904f3
Andrea <35015674+Sgridev@users.noreply.github.com>
2021-12-13 00:28:41 +0100

    create page

---
+++ Shared/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace KeizerPairing.Shared
+{
+    public class Player
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public int Rating { get; set; }
+
+        public string Presence { get; set; } = "Present";
+
+    }
+}
+
