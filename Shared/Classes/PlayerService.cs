9cbe69ae880769a561957d44daa5c51a3b1118c6
Andrea <35015674+Sgridev@users.noreply.github.com>
2021-12-14 01:16:08 +0100

    create tournament development

--- Shared/Classes/PlayerService.cs
+++ Shared/Classes/PlayerService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 
 namespace KeizerPairing.Shared
 {
@@ -14,6 +15,8 @@ namespace KeizerPairing.Shared
         public List<Pairing> CurrentPairings { get; set; } = new List<Pairing>();
 
         public int CurrentRoundNumber { get; set; } = 1;
+
+        public bool NextRoundDisabled { get; set; }
 
         public void Add(Player player)
         {
@@ -31,21 +34,25 @@ namespace KeizerPairing.Shared
         private void UpdatePairings()
         {
             CurrentPairings.Clear();
+            var playersByRating = CurrentPlayers.Where(x => x.Presence == "Present").OrderByDescending(x => x.Value).ToArray();
             if (CurrentRoundNumber == 1)
             {
-                var playersByRating = CurrentPlayers.Where(x => x.Presence == "Present").OrderByDescending(x => x.Rating).ToArray();
-                for (int i = 0; i < playersByRating.Count() - 1; i += 2)
+                playersByRating = CurrentPlayers.Where(x => x.Presence == "Present").OrderByDescending(x => x.Rating).ToArray();
+            }
+            for (int i = 0; i < playersByRating.Count() - 1; i += 2)
+            {
+                if (playersByRating[i + 1] != null)
                 {
-                    if (playersByRating[i + 1] != null)
-                    {
-                        Pairing pairing = new Pairing() { White = playersByRating[i], Black = playersByRating[i + 1] };
-                        CurrentPairings.Add(pairing);
-                    }
+                    Pairing pairing = new Pairing() { White = playersByRating[i], Black = playersByRating[i + 1] };
+                    CurrentPairings.Add(pairing);
                 }
             }
         }
+        //gli score si valutano con i value del round prima
+        //nel primo round si calcola con l'elo
         public void UpdateRankings()
         {
+
             if (CurrentRoundNumber == 1)
             {
                 int counter = 30;
@@ -61,25 +68,24 @@ namespace KeizerPairing.Shared
                     counter--;
                 }
 
-                if (CurrentPairings.Where(x => x.Result != null && x.Result != string.Empty).Count() > 0)
+                if (CurrentPairings.Where(x => x.Result != null && x.Result != string.Empty).Any())
                 {
 
                     foreach (var player in CurrentPlayers)
                     {
-                        Console.WriteLine(CurrentPairings.Where(x => (x.White.Number == player.Number))?.FirstOrDefault()?.White.Name);
-                        var playerPairing = CurrentPairings.Where(x => (x.White.Number == player.Number || x.Black.Number == player.Number) && x.Result != null && x.Result != string.Empty).FirstOrDefault();
-                        if (playerPairing != null)
+                        var opponentPlayer = CurrentPairings.Where(x => (x.White.Number == player.Number || x.Black.Number == player.Number) && x.Result != null && x.Result != string.Empty).FirstOrDefault();
+                        if (opponentPlayer != null)
                         {
-                            if (playerPairing.White.Number == player.Number)
+                            if (opponentPlayer.White.Number == player.Number)
                             {
-                                if (playerPairing.Result == "1/2 - 1/2")
+                                if (opponentPlayer.Result == "1/2 - 1/2")
                                 {
-                                    player.Score = player.Value + (playerPairing.Black.Value / 2);
+                                    player.Score = player.Value + (opponentPlayer.Black.Value / 2);
                                     player.Draws += 1;
                                 }
-                                else if (playerPairing.Result == "1 - 0")
+                                else if (opponentPlayer.Result == "1 - 0")
                                 {
-                                    player.Score = player.Value + playerPairing.Black.Value;
+                                    player.Score = player.Value + opponentPlayer.Black.Value;
                                     player.Wins += 1;
 
                                 }
@@ -88,15 +94,15 @@ namespace KeizerPairing.Shared
                             }
                             else
                             {
-                                if (playerPairing.Result == "1/2 - 1/2")
+                                if (opponentPlayer.Result == "1/2 - 1/2")
                                 {
-                                    player.Score = player.Value + (playerPairing.White.Value / 2);
+                                    player.Score = player.Value + (opponentPlayer.White.Value / 2);
                                     player.Draws += 1;
 
                                 }
-                                else if (playerPairing.Result == "0 - 1")
+                                else if (opponentPlayer.Result == "0 - 1")
                                 {
-                                    player.Score = player.Value + playerPairing.White.Value;
+                                    player.Score = player.Value + opponentPlayer.White.Value;
                                     player.Wins += 1;
                                 }
                                 else
@@ -116,6 +122,77 @@ namespace KeizerPairing.Shared
                     player.Position = position++;
                 }
             }
+            else
+            {
+                if (CurrentPairings.Where(x => x.Result != null && x.Result != string.Empty).Any())
+                {
+                    foreach (var player in CurrentPlayers)
+                    {
+                        var currentPairing = CurrentPairings.Where(x => (x.White.Number == player.Number || x.Black.Number == player.Number) && x.Result != null && x.Result != string.Empty).FirstOrDefault();
+                        if (currentPairing != null)
+                        {
+                            var lastRoundplayer = Rounds[CurrentRoundNumber - 2].Players.Where(x => x.Number == player.Number).FirstOrDefault();
+
+                            if (currentPairing.White.Number == player.Number)
+                            {
+                                int opponentLastRoundValue = Rounds[CurrentRoundNumber - 2].Players.Where(x => x.Number == currentPairing.Black.Number).FirstOrDefault().Value;
+                                if (currentPairing.Result == "1/2 - 1/2")
+                                {
+                                    player.Score = lastRoundplayer.Score + (opponentLastRoundValue / 2);
+                                    player.Draws = lastRoundplayer.Draws + 1;
+                                    player.Wins = lastRoundplayer.Wins;
+                                    player.Losses = lastRoundplayer.Losses;
+
+                                }
+                                else if (currentPairing.Result == "1 - 0")
+                                {
+                                    player.Score = lastRoundplayer.Score + opponentLastRoundValue;
+                                    player.Wins = lastRoundplayer.Wins + 1;
+                                    player.Losses = lastRoundplayer.Losses;
+                                    player.Draws = lastRoundplayer.Draws;
+
+                                }
+                                else
+                                {
+                                    player.Losses = lastRoundplayer.Losses + 1;
+                                    player.Wins = lastRoundplayer.Wins;
+                                    player.Draws = lastRoundplayer.Draws;
+
+                                }
+                            }
+                            else
+                            {
+                                int opponentLastRoundValue = Rounds[CurrentRoundNumber - 2].Players.Where(x => x.Number == currentPairing.White.Number).FirstOrDefault().Value;
+                                if (currentPairing.Result == "1/2 - 1/2")
+                                {
+                                    player.Score = lastRoundplayer.Score + (opponentLastRoundValue / 2);
+                                    player.Draws = lastRoundplayer.Draws + 1;
+                                    player.Losses = lastRoundplayer.Losses;
+                                    player.Draws = lastRoundplayer.Draws;
+
+                                }
+                                else if (currentPairing.Result == "0 - 1")
+                                {
+                                    player.Score = lastRoundplayer.Score + opponentLastRoundValue;
+                                    player.Wins = lastRoundplayer.Wins + 1;
+                                    player.Losses = lastRoundplayer.Losses;
+                                    player.Draws = lastRoundplayer.Draws;
+                                }
+                                else
+                                {
+                                    player.Losses = lastRoundplayer.Losses + 1;
+                                    player.Wins = lastRoundplayer.Wins;
+                                    player.Draws = lastRoundplayer.Draws;
+                                }
+
+                            }
+
+                        }
+                    }
+                }
+            }
+            NextRoundDisabled = CurrentPairings.Any(x => x.Result == null || x.Result == string.Empty);
+
             OnChange?.Invoke();
 
         }
@@ -128,8 +205,31 @@ namespace KeizerPairing.Shared
                 CurrentPlayers = playerService.CurrentPlayers;
                 CurrentPairings = playerService.CurrentPairings;
                 CurrentRoundNumber = playerService.CurrentRoundNumber;
+                NextRoundDisabled = playerService.NextRoundDisabled;
             }
         }
 
+        public void NextRound()
+        {
+
+            Round round = new Round();
+            round.RoundNumber = CurrentRoundNumber++;
+            round.Pairings = CurrentPairings.Clone();
+            round.Players = CurrentPlayers.Clone();
+            Rounds.Add(round);
+            NextRoundDisabled = true;
+            OnChange?.Invoke();
+            UpdatePairings();
+        }
+
+
+    }
+    public static class CloneExtensions
+    {
+        public static T Clone<T>(this T cloneable) where T : new()
+        {
+            var toJson = JsonSerializer.Serialize(cloneable);
+            return JsonSerializer.Deserialize<T>(toJson);
+        }
     }
 }
